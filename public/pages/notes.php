881dc282c5f49ec0a694b76a568fa79e3b1bacb4
Adil <adil.bouamrioune@gmail.com>
2023-04-26 19:20:21 +0200

    responsive correction main and sidebar

--- public/pages/notes.php
+++ public/pages/notes.php
@@ -57,6 +57,7 @@
         function openNav() {
             document.getElementById("sidebar").style.width = "20%";
             document.getElementById("main").style.marginLeft = "20%";
+            document.getElementById("main").style.marginTop = "-120px";
         }
 
         function closeNav() {
